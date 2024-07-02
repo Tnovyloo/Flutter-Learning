fdf69db69579370dc62fe1fcae1506893f23577e
Tomasz Nowak <76777800+Tnovyloo@users.noreply.github.com>
2024-07-02 20:59:06 +0200

    Creating project in Flutter

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:province_issue_tracker/pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // return const MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: const HomePage(),
+    // );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: const HomePage(),
+      
+    );
+  }
+}
